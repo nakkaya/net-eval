d3155fbf45a26b6cd948167bba458ef1d3dfb1a0
Tayssir John Gabbour <tjg@pentaside.org>
2010-03-03 22:52:19 +0100

    The user can plug-in her own handler if a connection error happened while firing a task. For example, her handler can try resending the failed task to other machines, to increase the chance of success.

--- src/net_eval.clj
+++ src/net_eval.clj
@@ -5,6 +5,7 @@
   (:use clojure.test)
   (:use clojure.contrib.server-socket)
   (:use clojure.contrib.str-utils)
+  (:use [clojure.contrib.error-kit :as errkit])
   (:use [clojure.contrib.duck-streams :only [reader writer]])
   (:import (java.net Socket)
 	   (java.net InetSocketAddress)))
@@ -47,24 +48,40 @@
     (net-write conn f)
     (read-string (re-sub  #".*=>" "" (net-read conn)))))
 
+(errkit/deferror connection-error []
+  "Raised when an exception occurred when executing task. task-pos
+  is the position of task within the sequence given to net-eval."
+  [task task-pos exception]
+  {:msg (str "Connection error: Task " task-pos " failed to execute.")
+   :unhandled (constantly nil)})
+
 (defn- fire-task
   "Connect to a slave, send the task and return the output, 
    swallow any errors."
-  [task]
-  (try
-   (let [[host port _ & args] task
-	 call (task 2)
-	 conn (connect host port)]
-     (with-open [_ (:socket @conn)]
-       (send-task conn call args)))
-   (catch Exception e)))
+  ([task]
+     (fire-task task 0))
+  ([task task-pos]
+     (try
+      (let [[host port _ & args] task
+            call (task 2)
+            conn (connect host port)]
+        (with-open [_ (:socket @conn)]
+          (send-task conn call args)))
+      (catch Exception e
+        (errkit/raise connection-error task task-pos e)))))
 
 (defn net-eval
   "Send tasks for evaluation, takes a vector of vectors containing 
-   host port and task."
+   host port and task.
+
+   Raises connection-error (using clojure.contrib.error-kit) if
+   an error occurred while executing a task."
   [tasks]  
-  (vec (for [task tasks]
-         (future (fire-task task)))))
+  (vec (map (fn [task task-pos]
+              (let [fire-task (errkit/rebind-fn fire-task)]
+                (future (fire-task task task-pos))))
+            tasks
+            (iterate inc 0))))
 
 (defn -main
   "Create a REPL server, waiting for incoming tasks."
@@ -100,7 +117,11 @@
   (is (= [1 2 3] (do (deftask atask [] [1 2 3])
                      (fire-task ["127.0.0.1" 9999 #'atask]))))
   (is (= nil (do (deftask atask [] [1 2 3])
-                 (fire-task ["127.0.1.1" 9999 #'atask])))))
+                 (fire-task ["127.0.1.1" 9999 #'atask]))))
+  (is (= [1 2 3] (do (deftask atask [] [1 2 3])
+                     (errkit/with-handler (fire-task ["127.0.1.1" 9999 #'atask])
+                       (handle connection-error [task task-pos exception]
+                         (continue-with (fire-task ["127.0.0.1" 9999 #'atask]))))))))
 
 (deftest test-net-eval
   (is (= [[1 2 3]] (do (deftask atask [] [1 2 3])
