8e8d96a124ad07bcec82eef19ba33c04040a541a
Nurullah Akkaya <nurullah@nakkaya.com>
2010-05-05 15:13:23 +0300

    add lein clojars plugin.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://nakkaya.com/net-eval.markdown"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
   :namespaces [net-eval]
   :main net_eval)
