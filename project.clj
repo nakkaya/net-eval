1afe4fb8b88b0b62ef07a906a52df2bea7457a75
Nurullah Akkaya <nurullah@nakkaya.com>
2010-08-20 21:24:36 +0300

    switch to 1.2.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject net-eval "0.0.1"
   :description "Simple distributed computing."
   :url "http://nakkaya.com/net-eval.markdown"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
   :namespaces [net-eval]
   :main net_eval)
