00e8eb6f14ff861c2a5341cdd3e9d303e8e2ef01
Nurullah Akkaya <nurullah@nakkaya.com>
2011-12-11 17:08:53 +0200

    remove lein-clojars plugin

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :url "http://nakkaya.com/net-eval.markdown"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
   :namespaces [net-eval]
   :main net_eval)
